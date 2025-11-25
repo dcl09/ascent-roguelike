464fb7455de42e249aec831574a642f1edd8840c
d <d>
2025-11-25 22:16:03 +0000

    Added HasStats and Movable interfaces

---
+++ src/main/java/model/entities/interfaces/Movable.java
@@ -0,0 +1,9 @@
+package model.entities.interfaces;
+
+import model.game.Position;
+
+public interface Movable {
+    boolean moveTo(Position newPosition);
+    boolean canMoveTo(Position position);
+    int getMovementSpeed(); // this might not be needed?
+}
