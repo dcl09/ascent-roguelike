ee1d5053d6c3dafd154e6a082a71ce657af0e74e
d <d>
2025-11-25 22:40:28 +0000

    Fixed constructor logic on entities

--- src/main/java/model/entities/Wall.java
+++ src/main/java/model/entities/Wall.java
@@ -3,8 +3,12 @@ package model.entities;
 import model.game.Position;
 
 public class Wall extends Entity{
+    protected char symbol;
+    protected String color;
 
-    public Wall(Position position, char symbol, String color) {
-        super(position, symbol, color);
+    public Wall (Position position, char symbol, String color) {
+        super(position);
+        super.symbol = symbol;
+        super.color = color;
     }
 }
