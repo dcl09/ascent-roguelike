bb234525a838ffb600108d0b5d90e39cde992aec
d <d>
2025-11-25 22:02:08 +0000

    Changed Entity constructor

--- src/main/java/model/entities/Wall.java
+++ src/main/java/model/entities/Wall.java
@@ -4,10 +4,7 @@ import model.game.Position;
 
 public class Wall extends Entity{
 
-    private char Symbol;
-
-    public Wall(Position position, char symbol) {
-        super(position);
-        Symbol = symbol;
+    public Wall(Position position, char symbol, String color) {
+        super(position, symbol, color);
     }
 }
