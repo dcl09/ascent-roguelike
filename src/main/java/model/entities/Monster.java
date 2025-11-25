99e66e7e14dc3d9a76f9089a3a44b3c980215537
d <d>
2025-11-25 15:39:59 +0000

    Add monster baseline

---
+++ src/main/java/model/entities/Monster.java
@@ -0,0 +1,12 @@
+package model.entities;
+
+import model.game.Position;
+
+public class Monster extends Entity {
+    /* todo: add monster attributes */
+
+    public Monster(Position position, char symbol, String color) {
+        super(position, symbol, color);
+    }
+
+}
