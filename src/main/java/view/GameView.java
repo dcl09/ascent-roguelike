20bc5f0142f12c3a8c2bbdd1fc1dcc4ab0967c52
NunoMCoimbra <nunomfcoimbra@gmail.com>
2025-11-25 15:46:45 +0000

    Added basic skeleton to GameView, GameModel, GameController, and Game.

--- src/main/java/view/GameView.java
+++ src/main/java/view/GameView.java
@@ -1,10 +1,65 @@
 package view;
 
-import controller.GameController;
+
+
+import com.googlecode.lanterna.TerminalSize;
+import com.googlecode.lanterna.TextCharacter;
+import com.googlecode.lanterna.screen.Screen;
+import com.googlecode.lanterna.screen.TerminalScreen;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import com.googlecode.lanterna.terminal.Terminal;
+import com.googlecode.lanterna.terminal.swing.AWTTerminalFontConfiguration;
+import com.googlecode.lanterna.terminal.swing.AWTTerminalFrame;
 import model.GameModel;
 
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+
 public class GameView {
-    private GameView view;
-    private GameController controller;
+    // todo: add new private variables and initialize in constructor.
     private GameModel model;
+
+    GameView(GameModel model){
+        this.model = model;
+    }
+
+    public void draw() throws IOException, FontFormatException, URISyntaxException {
+        URL resource = getClass().getClassLoader().getResource("square.ttf");
+        File fontFile = new File(resource.toURI());
+        Font font =  Font.createFont(Font.TRUETYPE_FONT, fontFile);
+
+        GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+        ge.registerFont(font);
+
+        DefaultTerminalFactory factory = new DefaultTerminalFactory();
+
+        Font loadedFont = font.deriveFont(Font.PLAIN, 25);
+        AWTTerminalFontConfiguration fontConfig = AWTTerminalFontConfiguration.newInstance(loadedFont);
+        factory.setTerminalEmulatorFontConfiguration(fontConfig);
+        factory.setForceAWTOverSwing(true);
+        factory.setInitialTerminalSize(new TerminalSize(40, 20));
+
+        Terminal terminal = factory.createTerminal();
+        ((AWTTerminalFrame)terminal).addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                e.getWindow().dispose();
+            }
+        });
+
+        Screen screen = new TerminalScreen(terminal);
+        screen.setCursorPosition(null);   // we don't need a cursor
+        screen.startScreen();             // screens must be started
+        screen.doResizeIfNecessary();     // resize screen if necessary
+
+        screen.setCharacter(10, 10, TextCharacter.fromCharacter('C')[0]);
+        screen.refresh();
+    }
+
 }
+
