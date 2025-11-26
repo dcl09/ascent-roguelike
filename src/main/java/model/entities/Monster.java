9f23dc890f6e20389316c048f06d5178f61b961d
dcl674 <up202306056@up.pt>
2025-11-26 22:11:02 +0000

    Implemented Movable in Monster

--- src/main/java/model/entities/Monster.java
+++ src/main/java/model/entities/Monster.java
@@ -2,9 +2,10 @@ package model.entities;
 
 import model.entities.components.Stats;
 import model.entities.interfaces.Combatant;
+import model.entities.interfaces.Movable;
 import model.game.Position;
 
-public class Monster extends Entity implements Combatant {
+public class Monster extends Entity implements Combatant, Movable {
     private final Stats stats;
 
     public Monster (Position position, char symbol, String color) {
@@ -15,5 +16,23 @@ public class Monster extends Entity implements Combatant {
     @Override
     public Stats getStats() {
         return stats;
+    }
+
+    @Override
+    public boolean moveTo(Position newPosition) {
+        if (!canMoveTo(newPosition)) return false;
+        setPosition(newPosition);
+        return true;
+    }
+
+    @Override
+    public boolean canMoveTo(Position position) {
+        //todo: Falta adicionar outras restrições para movimento
+        return isAlive() && position != null;
+    }
+
+    @Override
+    public int getMovementSpeed() {
+        return stats.getSpeed();
     }
 }
