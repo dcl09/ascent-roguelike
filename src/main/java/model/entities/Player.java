64c23358ba14f4e50e46c30b84514828577de76d
dcl09 <dfcal8@gmail.com>
2025-11-26 21:27:11 +0000

    Added Combatant implementation to Player

--- src/main/java/model/entities/Player.java
+++ src/main/java/model/entities/Player.java
@@ -1,15 +1,23 @@
 package model.entities;
 
+import model.entities.components.Stats;
+import model.entities.interfaces.Combatant;
 import model.game.Position;
 
-public class Player extends Entity {
-    /* todo: add player attributes */
+public class Player extends Entity implements Combatant {
+    Stats stats;
 
     protected char symbol;
     protected String color;
 
     public Player (Position position, char symbol, String color) {
         super(position, symbol, color);
+        this.stats = new Stats(100, 1, 1);
+    }
+
+    @Override
+    public Stats getStats() {
+        return stats;
     }
 
 }
