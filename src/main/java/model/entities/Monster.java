8603f19a8b3f7496b309e3bb5b228b06d1dd8d27
dcl674 <up202306056@up.pt>
2025-11-26 21:39:21 +0000

    Implemented Combatant in Monster

--- src/main/java/model/entities/Monster.java
+++ src/main/java/model/entities/Monster.java
@@ -1,12 +1,19 @@
 package model.entities;
 
+import model.entities.components.Stats;
+import model.entities.interfaces.Combatant;
 import model.game.Position;
 
-public class Monster extends Entity {
-    /* todo: add monster attributes */
+public class Monster extends Entity implements Combatant {
+    private final Stats stats;
 
     public Monster (Position position, char symbol, String color) {
         super(position, symbol, color);
+        this.stats = new Stats(90, 2, 2);
     }
 
+    @Override
+    public Stats getStats() {
+        return stats;
+    }
 }
