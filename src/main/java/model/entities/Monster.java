3ddf2f248f3ea35109c8de237e0400574ea60b6c
d <d>
2025-11-26 14:50:08 +0000

    Entity constructor logic fixed

--- src/main/java/model/entities/Monster.java
+++ src/main/java/model/entities/Monster.java
@@ -5,13 +5,8 @@ import model.game.Position;
 public class Monster extends Entity {
     /* todo: add monster attributes */
 
-    protected char symbol;
-    protected String color;
-
-    public Monster(Position position, char symbol, String color) {
-        super(position);
-        super.symbol = symbol;
-        super.color = color;
+    public Monster (Position position, char symbol, String color) {
+        super(position, symbol, color);
     }
 
 }
