b2f6432dbe1817a75e2ee0a08483ea3528601c19
dcl674 <up202306056@up.pt>
2025-11-27 11:55:34 +0000

    Changed Items to Item, Armour, HealthRestore and Weapon

---
+++ src/main/java/model/entities/items/Item.java
@@ -0,0 +1,20 @@
+package model.entities.items;
+
+public class Item {
+    //protected int level = 1; // Just an idea for now, no scaling implemented yet
+    protected String name;
+    protected Integer id; // For making choosing a random item easier
+
+    public Item(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+}
