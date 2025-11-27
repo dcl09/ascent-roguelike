8418d095946058f3e236386c6398b6bc11f0801d
d <d>
2025-11-27 22:14:41 +0000

    Implement per slot getter and setter

--- src/main/java/model/Inventory.java
+++ src/main/java/model/Inventory.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 public class Inventory {
     protected ArrayList<Item> items;
     /* protected int size; -> this might be useless so im leaving it commented */
-    
+
     public Inventory(ArrayList<Item> items) {
         this.items = items;
     }
@@ -14,4 +14,15 @@ public class Inventory {
     public Inventory() {
         /* Player starts with an empty inventory? */
     }
+    
+    /* todo: check what else to add here  */
+
+    public Item getItemAtSlot(int index) {
+        return items.get(index);
+    }
+
+    public void changeItemAtSlot(Item item, int index) {
+        items.set(index, item);
+    }
+
 }
