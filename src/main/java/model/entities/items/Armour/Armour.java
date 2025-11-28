71573efdd84442545c5e80314cd91ab7c35e66c8
dcl674 <up202306056@up.pt>
2025-11-28 21:13:20 +0000

    Added equip and unequip methods for armour

--- src/main/java/model/entities/items/Armour/Armour.java
+++ src/main/java/model/entities/items/Armour/Armour.java
@@ -1,6 +1,7 @@
 package model.entities.items.Armour;
 
 import model.entities.items.Item;
+import model.entities.Player;
 
 public class Armour extends Item {
     protected int bonusResistenceToDamage;
@@ -18,5 +19,13 @@ public class Armour extends Item {
 
     public int getBonusResistenceToDamage() {
         return bonusResistenceToDamage;
+    }
+
+    public void onEquip(Player player) {
+        player.getStats().addResistenceToDamage(bonusResistenceToDamage);
+    }
+
+    public void onUnequip(Player player) {
+        player.getStats().loseResistenceToDamage(bonusResistenceToDamage);
     }
 }
