0df0844096688231fea67a68dfd90a60344640a4
d <d>
2025-11-28 17:01:06 +0000

    Minor refactor to Position class

--- src/main/java/model/game/Position.java
+++ src/main/java/model/game/Position.java
@@ -1,10 +1,11 @@
+// ==================== POSITION CLASS ====================
 package model.game;
 
 import java.util.Objects;
 
 public class Position {
-    private int x;
-    private int y;
+    private final int x;
+    private final int y;
 
     public Position(int x, int y) {
         this.x = x;
@@ -18,14 +19,35 @@ public class Position {
     public int getY() {
         return y;
     }
+
+    public Position offset(int dx, int dy) {
+        return new Position(this.x + dx, this.y + dy);
+    }
+
+    public int distanceTo(Position other) {
+        return Math.abs(this.x - other.x) + Math.abs(this.y - other.y);
+    }
+
+    public boolean isAdjacentTo(Position other) {
+        return distanceTo(other) == 1;
+    }
+
     @Override
     public boolean equals(Object o) {
-        if (!(o instanceof Position position)) return false;
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Position position = (Position) o;
         return x == position.x && y == position.y;
     }
 
     @Override
     public int hashCode() {
         return Objects.hash(x, y);
+    }
+
+    /* tool for debugging/logging purposes? */
+    @Override
+    public String toString() {
+        return "(" + x + ", " + y + ")";
     }
 }
