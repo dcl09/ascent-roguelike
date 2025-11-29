e8a10607e4b786b6f6d8db25e59e111ee4a02536
dcl09 <dfcal8@gmail.com>
2025-11-29 19:54:05 +0000

    Added in Monster empty constructor, reset method and active flag

--- src/main/java/model/entities/Monster.java
+++ src/main/java/model/entities/Monster.java
@@ -7,10 +7,40 @@ import model.game.Position;
 
 public class Monster extends Entity implements Combatant, Movable {
     private final Stats stats;
+    private boolean active;
+
+    // Empty Constructor for pool
+    public Monster() {
+        super(new Position(0, 0), 'M', "RED");
+        this.stats = new Stats(90, 2, 2);
+        this.active = false;
+    }
 
     public Monster (Position position, char symbol, String color) {
         super(position, symbol, color);
         this.stats = new Stats(90, 2, 2);
+        this.active = true;
+    }
+
+    public void activate() {
+        this.active = true;
+    }
+
+    public void deactivate() {
+        this.active = false;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void reset(Position position, char symbol, String color,
+                      int health, int damage, int speed) {
+        setPosition(position);
+        setSymbol(symbol);
+        setColor(color);
+        stats.reset(health, damage, speed);
+        this.active = true;
     }
 
     @Override
