b313a8b852be2ca15cb31d1fc0990a3f13b48ab1
dcl674 <up202306056@up.pt>
2025-11-29 20:03:09 +0000

    Created a MonsterPool

---
+++ src/main/java/model/entities/pools/MonsterPool.java
@@ -0,0 +1,42 @@
+package model.entities.pools;
+
+import model.entities.Monster;
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class MonsterPool {
+    private static MonsterPool instance;
+    private final Deque<Monster> pool;
+    private static final int SIZE = 30;
+
+    private MonsterPool() {
+        pool = new ArrayDeque<>(SIZE);
+        for (int i = 0; i < SIZE; i++) {
+            pool.push(new Monster());
+        }
+    }
+
+    public static MonsterPool getInstance() {
+        if (instance == null) {
+            instance = new MonsterPool();
+        }
+        return instance;
+    }
+
+    public Monster acquire() {
+        if (pool.isEmpty()) return null;
+        Monster m = pool.pop();
+        m.activate();
+        return m;
+    }
+
+    public void release(Monster monster) {
+        if (monster == null) return;
+        monster.deactivate();
+        pool.push(monster);
+    }
+
+    public int available() {
+        return pool.size();
+    }
+}
