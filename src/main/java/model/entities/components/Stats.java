ad44d521f7e57179e59d245d9bf618c1ab64d475
dcl674 <up202306056@up.pt>
2025-11-29 18:53:01 +0000

    Added reset method to reset Stats to certain values

--- src/main/java/model/entities/components/Stats.java
+++ src/main/java/model/entities/components/Stats.java
@@ -2,12 +2,22 @@ package model.entities.components;
 
 public class Stats {
     private int health;
-    private final int maxHealth;
+    private int maxHealth;
     private int damage;
     private int speed;
     private int resistanceToDamage;
 
     public Stats(int maxHealth, int damage, int speed) {
+        this.maxHealth = maxHealth;
+        this.health = maxHealth;
+        this.damage = damage;
+        this.speed = speed;
+        this.resistanceToDamage = 0;
+    }
+
+    //Method to help reset in Pools
+
+    public void reset(int maxHealth, int damage, int speed) {
         this.maxHealth = maxHealth;
         this.health = maxHealth;
         this.damage = damage;
