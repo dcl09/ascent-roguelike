db08292bb0638d8d1d3e6c30919f9d82cf0b4cc6
NunoMCoimbra <nunomfcoimbra@gmail.com>
2025-11-30 01:11:39 +0000

    Added barebones GameModel for first submission

--- src/main/java/model/GameModel.java
+++ src/main/java/model/GameModel.java
@@ -1,43 +1,31 @@
 package model;
 
-import com.googlecode.lanterna.input.KeyStroke;
-import com.googlecode.lanterna.input.KeyType;
-import model.entities.Player;
-import model.entities.Monster;
-import model.entities.Wall;
-import model.entities.Chest;
+import model.entities.*;
+import model.game.*;
+import model.game.level.BaseplateBuilder;
+
 
 import java.io.IOException;
 import java.util.List;
 
 public class GameModel {
+    private final Level level;
     private final Player player;
-    private final List<Monster> monsters;
-    private final List<Wall> walls;
-    private final List<Chest> chests;
+
     /* todo: add list of items, inventory and other stuff here */
 
-    public GameModel(Player player, List<Monster> monsters, List<Wall> walls, List<Chest> chests) {
+    public GameModel(Player player) {
         this.player = player;
-        this.monsters = monsters;
-        this.walls = walls;
-        this.chests = chests;
+        BaseplateBuilder builder = new BaseplateBuilder(50, 50 ,4);
+        this.level = builder.createLevel();
     }
 
     public Player getPlayer() {
         return player;
     }
 
-    public List<Monster> getMonsters() {
-        return monsters;
-    }
-
-    public List<Wall> getWalls() {
-        return walls;
-    }
-
-    public List<Chest> getChests() {
-        return chests;
+    public Level getLevel() {
+        return level;
     }
 
 }
