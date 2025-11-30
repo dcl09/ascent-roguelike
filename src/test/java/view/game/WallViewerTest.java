c657314f2ee2c430ed039958d44303c7707ca31c
d <d>
2025-11-30 15:00:57 +0000

    Added baseline WallViewerTest tester

---
+++ src/test/java/view/game/WallViewerTest.java
@@ -0,0 +1,28 @@
+package view.game;
+
+import gui.GUI;
+import model.entities.Wall;
+import model.game.Position;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+class WallViewerTest {
+    private WallViewer wallViewer;
+    private GUI gui;
+
+    @BeforeEach
+    void setup() {
+        wallViewer = new WallViewer();
+        gui = Mockito.mock(GUI.class);
+    }
+
+    @Test
+    void drawWall() {
+        Wall wall = new Wall(new Position(1, 2));
+        wallViewer.draw(wall, gui);
+        Mockito.verify(gui).drawChar(1, 2, '#', "WHITE");
+    }
+
+    
+}
