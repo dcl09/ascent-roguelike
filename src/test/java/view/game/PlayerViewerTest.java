f8cf53bc47251265a6b08299d431012302443b73
d <d>
2025-11-30 14:17:18 +0000

    Started implementing Mockito testing on PlayerViewerTest

--- src/test/java/view/game/PlayerViewerTest.java
+++ src/test/java/view/game/PlayerViewerTest.java
@@ -1,13 +1,23 @@
 package view.game;
 
+import gui.GUI;
 import model.entities.Player;
 import model.game.Position;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+import org.mockito.Mockito;
 
 class PlayerViewerTest {
     private PlayerViewer playerViewer;
+    private GUI gui;
+    private Player player;
 
-    
+    @BeforeEach
+    void setup() {
+        playerViewer = new PlayerViewer();
+        gui = Mockito.mock(GUI.class);
+        player = new Player(new Position(5,8));
+    }
+
 }
