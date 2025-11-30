8e0ee5cd08c0f8d27158a31c450349665e2757ad
NunoMCoimbra <nunomfcoimbra@gmail.com>
2025-11-30 01:40:06 +0000

    Changed movable

--- src/main/java/model/entities/interfaces/Movable.java
+++ src/main/java/model/entities/interfaces/Movable.java
@@ -1,8 +1,8 @@
 package model.entities.interfaces;
 
-import model.game.Position;
-
 public interface Movable {
-    //boolean moveTo(Position newPosition);
-    boolean canMoveTo(Position position);
+    void moveUp();
+    void moveDown();
+    void moveLeft();
+    void moveRight();
 }
