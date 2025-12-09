47fe43d1eeb0bd8a4befa64cd697476d3b34b503
NunoMCoimbra <nunomfcoimbra@gmail.com>
2025-12-09 22:21:18 +0000

    Deleted old GameView, completed Viewer for current features

---
+++ src/main/java/view/Viewer.java
@@ -0,0 +1,25 @@
+package view;
+
+import gui.GUI;
+
+import java.io.IOException;
+
+public abstract class Viewer<T> {
+    private final T model;
+
+    public Viewer(T model) {
+        this.model = model;
+    }
+
+    public T getModel() {
+        return model;
+    }
+
+    public void draw(GUI gui) throws IOException {
+        gui.clear();
+        drawEntities(gui);
+        gui.refresh();
+    }
+
+    protected abstract void drawEntities(GUI gui);
+}
