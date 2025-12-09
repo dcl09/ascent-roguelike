f1742e91be0b8d2c07d1ebcc57e5ea351d1a1ad2
dcl09 <dfcal8@gmail.com>
2025-12-09 18:09:19 +0000

    Implement chests and interaction system

--- src/main/java/controller/GameController.java
+++ src/main/java/controller/GameController.java
@@ -1,77 +1,111 @@
 package controller;
 
 import model.GameModel;
+import model.entities.Chest;
 import model.entities.Player;
 import model.game.Position;
+import model.game.level.Level;
 import view.GameView;
 import gui.*;
 
 import java.io.IOException;
 
 public class GameController {
-    // todo: add new private variables and initialize in constructor.
     private final GameModel model;
     private final GameView view;
     private final GUI gui;
 
-    public GameController(GameView view, GameModel model, GUI gui){
+    public GameController(GameView view, GameModel model, GUI gui) {
         this.view = view;
         this.model = model;
         this.gui = gui;
     }
+
     public void run() throws IOException {
         view.draw(gui);
 
-        while (true){
+        while (true) {
             ACTION curraction = gui.processKey();
             if (curraction == ACTION.QUIT)
                 break;
             if (curraction != null)
                 processAction(curraction);
-            // move monsters
             view.draw(gui);
         }
 
         gui.close();
     }
 
-    public void processAction(ACTION action){
+    public void processAction(ACTION action) {
         Player player = model.getPlayer();
         Position currentPos = player.getPosition();
         Position newPos = null;
 
-
         switch (action) {
             case UP:
                 newPos = currentPos.getUp();
-                if (conditionals(newPos))
+                if (canMoveTo(newPos)) {
                     player.moveUp();
+                }
                 break;
             case DOWN:
                 newPos = currentPos.getDown();
-                if (conditionals(newPos))
+                if (canMoveTo(newPos)) {
                     player.moveDown();
+                }
                 break;
             case LEFT:
                 newPos = currentPos.getLeft();
-                if (conditionals(newPos))
+                if (canMoveTo(newPos)) {
                     player.moveLeft();
+                }
                 break;
             case RIGHT:
                 newPos = currentPos.getRight();
-                if (conditionals(newPos))
+                if (canMoveTo(newPos)) {
                     player.moveRight();
+                }
+                break;
+            case INTERACT:
+                tryInteract(player);
                 break;
             default:
                 break;
         }
     }
 
-    public boolean conditionals(Position position) {
-        if (position == null)
+    private boolean canMoveTo(Position position) {
+        if (position == null) {
             return false;
-        return !model.getLevel().isWall(position);
+        }
+        Level level = model.getLevel();
+        if (level.isWall(position)) {
+            return false;
+        }
+        if (level.isMonster(position)) {
+            return false;
+        }
+        return true;
     }
 
-}
+    private void tryInteract(Player player) {
+        Position playerPos = player.getPosition();
+        Level level = model.getLevel();
 
+        // Verificar chests adjacentes
+        Position[] adjacentPositions = {
+                playerPos.getUp(),
+                playerPos.getDown(),
+                playerPos.getLeft(),
+                playerPos.getRight()
+        };
+
+        for (Position pos : adjacentPositions) {
+            Chest chest = level.getChestAt(pos);
+            if (chest != null && chest.canInteract()) {
+                player.interactWith(chest);
+                return;
+            }
+        }
+    }
+}
--- src/main/java/model/game/level/Level.java
+++ src/main/java/model/game/level/Level.java
@@ -1,24 +1,28 @@
 package model.game.level;
+
 import model.entities.*;
 import model.game.Position;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
+import java.util.HashMap;
 
 public class Level {
     private final int width;
     private final int height;
 
     private Player player;
-
     private List<Monster> monsters;
     private List<Chest> chests;
     private Map<Position, Wall> walls;
 
-    public Level(int width, int height){
+    public Level(int width, int height) {
         this.width = width;
         this.height = height;
+        this.monsters = new ArrayList<>();
+        this.chests = new ArrayList<>();
+        this.walls = new HashMap<>();
     }
 
     public int getWidth() {
@@ -29,11 +33,11 @@ public class Level {
         return height;
     }
 
-    public Player getPlayer(){
+    public Player getPlayer() {
         return player;
     }
 
-    public void setPlayer(Player player){
+    public void setPlayer(Player player) {
         this.player = player;
     }
 
@@ -42,15 +46,15 @@ public class Level {
     }
 
     public void setMonsters(List<Monster> monsters) {
-        this.monsters = monsters;
+        this.monsters = monsters != null ? monsters : new ArrayList<>();
     }
 
-    public List<Chest> getChests(){
+    public List<Chest> getChests() {
         return chests;
     }
 
-    public void setChests(List<Chest> chests){
-        this.chests = chests;
+    public void setChests(List<Chest> chests) {
+        this.chests = chests != null ? chests : new ArrayList<>();
     }
 
     public List<Wall> getWalls() {
@@ -58,8 +62,12 @@ public class Level {
     }
 
     public void setWalls(List<Wall> walls) {
-        this.walls = walls.stream()
-                .collect(Collectors.toMap(Wall::getPosition, wall -> wall));
+        this.walls = new HashMap<>();
+        if (walls != null) {
+            for (Wall wall : walls) {
+                this.walls.put(wall.getPosition(), wall);
+            }
+        }
     }
 
     public boolean isWall(Position position) {
@@ -67,9 +75,29 @@ public class Level {
     }
 
     public boolean isMonster(Position position) {
-        for (Monster monster : monsters)
-            if (monster.getPosition().equals(position))
+        for (Monster monster : monsters) {
+            if (monster.getPosition().equals(position)) {
                 return true;
+            }
+        }
         return false;
+    }
+
+    public boolean isChest(Position position) {
+        for (Chest chest : chests) {
+            if (chest.getPosition().equals(position)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public Chest getChestAt(Position position) {
+        for (Chest chest : chests) {
+            if (chest.getPosition().equals(position)) {
+                return chest;
+            }
+        }
+        return null;
     }
 }
