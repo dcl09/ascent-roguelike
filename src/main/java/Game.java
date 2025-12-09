47815cbd1c79b8a9b12bea43a42cb9291f5d1a91
d <d>
2025-12-09 15:27:09 +0000

    Explained some stuff

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -21,9 +21,16 @@ public class Game {
         GameView view = new GameView(model);
         GameController controller = new GameController(view, model, gui);
 
+        /* bad example of game loop */
         controller.placeholder_refresh_GUI();
         while (true) {
             /* game loop here with placeholders */
+            /* todo: we need to abstract the input processing,
+                rendering and time (delta) updating to separate functions that all run independently in the game loop
+                but constrained to the tick/time defined, with sleeps if they are ahead of schedule
+                also calculate how optimized the game is to make sure our time window is slightly above the slowest frame draw time
+                to make sure we dont go into have trouble
+             */
             ACTION curraction = controller.placeholder_processKey();
             if (curraction == ACTION.QUIT) break;
             else if (curraction != null)
@@ -31,5 +38,6 @@ public class Game {
             controller.placeholder_refresh_GUI();
         }
         controller.placeholder_close_GUI();
+        /* */
     }
 }
