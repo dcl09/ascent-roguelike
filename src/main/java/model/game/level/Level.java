07a9fa209d6e81c7ef2cf76038b17ec2054a5333
dcl09 <dfcal8@gmail.com>
2025-12-12 11:45:48 +0000

    Add Door support to Level model and builders

--- src/main/java/model/game/level/Level.java
+++ src/main/java/model/game/level/Level.java
@@ -2,11 +2,8 @@ package model.game.level;
 
 import model.entities.*;
 import model.game.Position;
-
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-import java.util.HashMap;
 
 public class Level {
     private final int width;
@@ -15,14 +12,16 @@ public class Level {
     private Player player;
     private List<Monster> monsters;
     private List<Chest> chests;
-    private Map<Position, Wall> walls;
+    private List<Wall> walls;
+    private List<Door> doors;
 
     public Level(int width, int height) {
         this.width = width;
         this.height = height;
         this.monsters = new ArrayList<>();
         this.chests = new ArrayList<>();
-        this.walls = new HashMap<>();
+        this.walls = new ArrayList<>();
+        this.doors = new ArrayList<>();
     }
 
     public int getWidth() {
@@ -46,7 +45,7 @@ public class Level {
     }
 
     public void setMonsters(List<Monster> monsters) {
-        this.monsters = monsters != null ? monsters : new ArrayList<>();
+        this.monsters = monsters;
     }
 
     public List<Chest> getChests() {
@@ -54,49 +53,63 @@ public class Level {
     }
 
     public void setChests(List<Chest> chests) {
-        this.chests = chests != null ? chests : new ArrayList<>();
+        this.chests = chests;
     }
 
     public List<Wall> getWalls() {
-        return List.copyOf(walls.values());
+        return walls;
     }
 
     public void setWalls(List<Wall> walls) {
-        this.walls = new HashMap<>();
-        if (walls != null) {
-            for (Wall wall : walls) {
-                this.walls.put(wall.getPosition(), wall);
-            }
-        }
+        this.walls = walls;
+    }
+
+    public List<Door> getDoors() {
+        return doors;
+    }
+
+    public void setDoors(List<Door> doors) {
+        this.doors = doors;
     }
 
     public boolean isWall(Position position) {
-        return walls.containsKey(position);
+        for (Wall wall : walls) {
+            if (wall.getPosition().equals(position)) return true;
+        }
+        return false;
     }
 
     public boolean isMonster(Position position) {
         for (Monster monster : monsters) {
-            if (monster.getPosition().equals(position)) {
-                return true;
-            }
+            if (monster.getPosition().equals(position)) return true;
         }
         return false;
     }
 
     public boolean isChest(Position position) {
         for (Chest chest : chests) {
-            if (chest.getPosition().equals(position)) {
-                return true;
-            }
+            if (chest.getPosition().equals(position)) return true;
         }
         return false;
     }
 
     public Chest getChestAt(Position position) {
         for (Chest chest : chests) {
-            if (chest.getPosition().equals(position)) {
-                return chest;
-            }
+            if (chest.getPosition().equals(position)) return chest;
+        }
+        return null;
+    }
+
+    public boolean isDoor(Position position) {
+        for (Door door : doors) {
+            if (door.getPosition().equals(position)) return true;
+        }
+        return false;
+    }
+
+    public Door getDoorAt(Position position) {
+        for (Door door : doors) {
+            if (door.getPosition().equals(position)) return door;
         }
         return null;
     }
