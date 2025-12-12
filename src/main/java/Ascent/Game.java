a4cbdc9b764c3ad963f54dced4b6371f45521b6e
d <d>
2025-12-12 11:46:49 +0000

    Added primitive time normalization to the game loop

--- src/main/java/Ascent/Game.java
+++ src/main/java/Ascent/Game.java
@@ -21,6 +21,7 @@ public class Game {
         this.stateStack = new Stack<>();
         this.player = new Player(new Position(20, 10));
     }
+
     public static void main(String[] args) throws IOException {
         new Game().start();
     }
@@ -35,11 +36,31 @@ public class Game {
 
     public void start() throws IOException {
         // todo: implement time in this funct & implement menus
+
+        /* some "inspiration" from hero-solid for time functions */
+
+        int FPS = 60; // for now, 10 fps feels sluggish and unnecessary, it slows the game down for no reason (computations are fast, for now)
+        int frameTime = 1000 / FPS;
+
         // stateStack.push(new StartMenuState (new StartMenu))
         stateStack.push(new GameState(new BaseplateBuilder(40, 20, 4, 1).createFloor(player)));
         while (!stateStack.empty()) {
+            /* get time at the start of the loop, do the step, get the end time and the difference is the elapsed time */
+            long startTime = System.currentTimeMillis();
+
             State<?> currState = stateStack.peek();
             currState.step(this, gui);
+
+            long elapsedTime = System.currentTimeMillis() - startTime;
+
+            /* sleepTime is just the leftover time for the current iteration */
+            long sleepTime = frameTime - elapsedTime;
+
+            try {
+                if (sleepTime > 0) Thread.sleep(sleepTime);
+            } catch (InterruptedException e) {
+                // throw new RuntimeException(e); -> this is irrelevant, if time is 0 (or less for some quantum reason) we just don't sleep and ignore the error
+            }
         }
         gui.close();
     }
