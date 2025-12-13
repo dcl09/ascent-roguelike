85c2b987c44f1f5ff76316468d6a161ab7032eb2
dcl09 <dfcal8@gmail.com>
2025-12-13 14:45:54 +0000

    Implemented moveMonster on Floor and made it change the monster map in Level

--- src/main/java/Ascent/controller/game/MonsterController.java
+++ src/main/java/Ascent/controller/game/MonsterController.java
@@ -6,25 +6,24 @@ import Ascent.model.entities.Monster;
 import Ascent.model.game.Position;
 import Ascent.model.game.floor.Floor;
 
+import java.util.Collection;
+
 public class MonsterController extends GameController {
 // Todo: Implement time and related checks & import game
     private long lastMovement;
 
     public MonsterController(Floor floor){ super(floor); }
 
-    public void moveMonster(Monster monster, Position position){
-        if (!getModel().isWall(position)){
-            if (getModel().getPlayer().getPosition().equals(position));
-                // hurt player in position
-            else monster.setPosition(position);
-        }
+    public boolean moveMonster(Position initialPosition, Position finalPosition){
+        return getModel().moveMonster(initialPosition, finalPosition);
     }
 
     public void step(Game game, ACTION action/*, long time*/){
         // Implement check if enough time has elapsed since last movement
-        for (Monster monster : getModel().getMonsters()) {
+        Collection<Monster> monsters = getModel().getMonsters();
+        for (Monster monster : monsters) {
             // Implement check if monster is aware of player, else random movement
-            moveMonster(monster, monster.getPosition().getRandomAdjacent());
+            moveMonster(monster.getPosition(), monster.getPosition().getRandomAdjacent());
         }
     }
 }
