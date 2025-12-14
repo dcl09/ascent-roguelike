13e173c063918f9c95b4728d9e10e0883007cc66
dcl09 <dfcal8@gmail.com>
2025-12-14 12:03:20 +0000

    Changed LOOKING enum to clean code

--- src/main/java/Ascent/model/entities/components/LOOKING.java
+++ src/main/java/Ascent/model/entities/components/LOOKING.java
@@ -1,5 +1,39 @@
 package Ascent.model.entities.components;
 
+import Ascent.model.game.Position;
+
 public enum LOOKING {
-    UP, DOWN, LEFT, RIGHT
+
+    UP(0, -1, '▲'),
+    DOWN(0, 1, '▼'),
+    LEFT(-1, 0, '◄'),
+    RIGHT(1, 0, '►');
+
+    private final int deltaX;
+    private final int deltaY;
+    private final char symbol;
+
+    LOOKING(int deltaX, int deltaY, char symbol) {
+        this.deltaX = deltaX;
+        this.deltaY = deltaY;
+        this.symbol = symbol;
+    }
+
+    public int getDeltaX() {
+        return deltaX;
+    }
+
+    public int getDeltaY() {
+        return deltaY;
+    }
+
+    public char getSymbol() {
+        return symbol;
+    }
+
+    public Position move(Position initPosition) {
+        return new Position(
+                initPosition.getX() + this.deltaX,
+                initPosition.getY() + this.deltaY);
+    }
 }
