0d21e8fe412f6f1352a11771c71ab0aa2339c33c
dcl674 <up202306056@up.pt>
2025-12-14 15:12:48 +0000

    Added interact methods to door

--- src/main/java/Ascent/model/entities/Door.java
+++ src/main/java/Ascent/model/entities/Door.java
@@ -1,8 +1,10 @@
 package Ascent.model.entities;
 
+import Ascent.model.entities.interfaces.Interactable;
+import Ascent.model.entities.interfaces.Interactor;
 import Ascent.model.game.Position;
 
-public class Door extends Entity {
+public class Door extends Entity implements Interactable{
     private static final char CLOSED_SYMBOL = 'D';
     private static final char OPEN_SYMBOL = 'd';
 
@@ -10,12 +12,8 @@ public class Door extends Entity {
 
     public Door(Position position) {
         super(position, CLOSED_SYMBOL, "WHITE");
-        this.isOpen = true;
-        updateSymbol();
-    }
-
-    private void updateSymbol() {
-        this.symbol = isOpen ? OPEN_SYMBOL : CLOSED_SYMBOL;
+        this.isOpen = false;
+        this.symbol = CLOSED_SYMBOL;
     }
 
     public boolean isOpen() {
@@ -24,11 +22,25 @@ public class Door extends Entity {
 
     public void close() {
         this.isOpen = false;
-        updateSymbol();
+        this.symbol = CLOSED_SYMBOL;
     }
 
     public void open() {
         this.isOpen = true;
-        updateSymbol();
+        this.symbol = OPEN_SYMBOL;
+    }
+
+    @Override
+    public boolean canInteract() {
+        return true;
+    }
+
+    @Override
+    public void interact(Interactor interactor) {
+        if (isOpen) {
+            close();
+        } else {
+            open();
+        }
     }
 }
