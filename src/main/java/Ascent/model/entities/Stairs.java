0273510f292d74a7e02a47c9509512ed72f3a503
NunoMCoimbra <nunomfcoimbra@gmail.com>
2025-12-15 17:50:54 +0000

    Added Stairs entity

---
+++ src/main/java/Ascent/model/entities/Stairs.java
@@ -0,0 +1,11 @@
+package Ascent.model.entities;
+
+import Ascent.model.game.Position;
+
+public class Stairs extends Entity {
+    public Stairs(Position position) {
+        super(position, '=', "WHITE_BRIGHT");
+    }
+}
+
+
