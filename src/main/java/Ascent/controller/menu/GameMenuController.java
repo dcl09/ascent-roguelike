f0d7e21e5bf071f2f6a8056afbdf2b035e929cef
dcl674 <up202306056@up.pt>
2025-12-15 20:15:08 +0000

    Update controllers with new action handling and chest logic

--- src/main/java/Ascent/controller/menu/GameMenuController.java
+++ src/main/java/Ascent/controller/menu/GameMenuController.java
@@ -8,24 +8,25 @@ import Ascent.model.menu.GameMenu;
 import java.io.IOException;
 
 public class GameMenuController extends Controller<GameMenu> {
-    public GameMenuController(GameMenu gameMenu){ super(gameMenu);}
+    public GameMenuController(GameMenu gameMenu) {
+        super(gameMenu);
+    }
 
     @Override
     public void step(Game game, ACTION action, long time) throws IOException {
-        switch (action){
-            case UP:
-                getModel().prevEntry();
-                break;
-            case DOWN:
-                getModel().nextEntry();
-                break;
-            case SELECT:
+        switch (action) {
+            case UP -> getModel().prevEntry();
+            case DOWN -> getModel().nextEntry();
+            case SELECT -> {
                 if (getModel().getCurrEntry().equals("MAIN MENU")) {
                     game.popState();
                     game.popState();
                 }
                 if (getModel().getCurrEntry().equals("RESUME"))
                     game.popState();
+            }
+            default -> {
+            }
         }
     }
 }
