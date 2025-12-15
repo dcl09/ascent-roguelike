49453282f3c3b28c29217ac35a2dd81b886aed2c
d <d>
2025-12-15 00:58:12 +0000

    Fix: change invalid color codes

--- src/main/java/Ascent/model/entities/monster/MonsterType.java
+++ src/main/java/Ascent/model/entities/monster/MonsterType.java
@@ -4,10 +4,10 @@ import Ascent.model.entities.components.Stats;
 
 public enum MonsterType {
     GOBLIN('g', "GREEN", 30, 3, 3),
-    ORC('O', "DARK_GREEN", 0, 8, 2),
+    ORC('O', "#154734", 0, 8, 2),
     DRAGON('R', "RED", 150, 20, 4),
     SKELETON('s', "WHITE", 25, 5, 2),
-    ZOMBIE('Z', "GREY", 50, 6, 1);
+    ZOMBIE('Z', "#808080 ", 50, 6, 1);
 
     private final char symbol;
     private final String color;
