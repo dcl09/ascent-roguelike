c37b780c10ce4f92ff44fe87958e2f444faf73aa
d <d>
2025-12-28 23:32:07 +0000

    Complete overhaul of PlayerControllerTest

--- src/test/java/ascent/controller/PlayerControllerTest.java
+++ src/test/java/ascent/controller/PlayerControllerTest.java
@@ -4,14 +4,13 @@ import ascent.Game;
 import ascent.controller.game.PlayerController;
 import ascent.gui.ACTION;
 import ascent.gui.GUI;
-import ascent.model.entities.Chest;
-import ascent.model.entities.Door;
-import ascent.model.entities.Player;
-import ascent.model.entities.Wall;
+import ascent.model.entities.*;
+import ascent.model.entities.components.LOOKING;
 import ascent.model.entities.monster.Monster;
 import ascent.model.game.Position;
 import ascent.model.game.floor.FileLevelFactory;
 import ascent.model.game.floor.Floor;
+import ascent.model.items.armour.ArmourSlot;
 import ascent.model.menu.Endscreen;
 import ascent.model.menu.GameMenu;
 import ascent.state.EndscreenState;
@@ -22,9 +21,15 @@ import ascent.view.Viewer;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import static org.mockito.Mockito.*;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import static org.mockito.ArgumentMatchers.anyLong;
 
 import java.io.IOException;
 import java.util.Arrays;
@@ -35,48 +40,175 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.mockito.Mockito.*;
 
+@ExtendWith(MockitoExtension.class)
 class PlayerControllerTest {
-    private PlayerController controller;
-    private Player player;
-    private FileLevelFactory fileLevelFactory;
-    private Floor floor;
+    @Mock Floor floor;
+    @Mock Player player;
+    @Mock Game game;
+
+    @InjectMocks
+    PlayerController controller;
 
     @BeforeEach
-    void setUp() throws IOException {
-        player = new Player(new Position(6,3));
-        fileLevelFactory = new FileLevelFactory();
-        floor = fileLevelFactory.createLevel(1, player);
-
-        floor.setPlayer(player);
-
-        floor.setWalls(List.of());
-        floor.setMonsters(List.of());
-
-        controller = new PlayerController(floor);
-    }
-
-    // start position is (6,3) -> from level 1
-    @Test
-    void heroMovesToEmptyPosition() {
-        controller.movePlayer(new Position(5,4), 200);
-        assertEquals(new Position(5, 4), player.getPosition());
+    void setUp() {
+        when(floor.getPlayer()).thenReturn(player);
     }
 
     @Test
-    void heroDoesNotMoveThroughObstacles() {
-        // wall blocks
-        floor.setWalls(List.of(new Wall(new Position(5,3))));
-        controller.movePlayer(new Position(5,3), 200);
-        assertEquals(new Position(6, 3), player.getPosition());
+    void playerMovementBlockedWhenCooldownActive() {
+        when(player.getMovementSpeed()).thenReturn(3);
+        when(player.moveToward(LOOKING.UP)).thenReturn(new Position(1,1));
 
-        // chest blocks
-        floor.setChests(List.of(new Chest(new Position(6,4), "YELLOW")));
-        controller.movePlayer(new Position(6,4), 200);
-        assertEquals(new Position(6, 3), player.getPosition());
+        controller.step(game, ACTION.UP, 1000);
+        controller.step(game, ACTION.UP, 1050);
 
-        // door at default state (closed) blocks
-        floor.setDoors(List.of(new Door(new Position(7,3))));
-        controller.movePlayer(new Position(7,3), 200);
-        assertEquals(new Position(6, 3), player.getPosition());
+        verify(floor, times(1)).movePlayer(any(Position.class), eq(1000L));
     }
+
+    @Test
+    void playerMovementAllowedWhenCooldownInactive() {
+        when(player.getMovementSpeed()).thenReturn(3);
+        when(player.moveToward(LOOKING.UP)).thenReturn(new Position(1,1));
+
+        controller.step(game, ACTION.UP, 1000L);
+        controller.step(game, ACTION.UP, 1101L);
+
+        verify(floor, times(1)).movePlayer(any(Position.class), eq(1000L));
+        verify(floor, times(1)).movePlayer(any(Position.class), eq(1101L));
+    }
+
+    @Test
+    void playerLooksUpCorrectly() {
+        controller.step(game, ACTION.LOOK_UP, 1000L);
+        verify(player, times(1)).setLookingDirection(LOOKING.UP);
+    }
+
+    @Test
+    void playerLooksDownCorrectly() {
+        controller.step(game, ACTION.LOOK_DOWN, 1000L);
+        verify(player, times(1)).setLookingDirection(LOOKING.DOWN);
+    }
+
+    @Test
+    void playerLooksLeftCorrectly() {
+        controller.step(game, ACTION.LOOK_LEFT, 1000L);
+        verify(player, times(1)).setLookingDirection(LOOKING.LEFT);
+    }
+
+    @Test
+    void playerLooksRightCorrectly() {
+        controller.step(game, ACTION.LOOK_RIGHT, 1000L);
+        verify(player, times(1)).setLookingDirection(LOOKING.RIGHT);
+    }
+
+    @Test
+    void playerMovesUpCorrectly() {
+        when(player.getMovementSpeed()).thenReturn(3);
+        when(player.moveToward(LOOKING.UP)).thenReturn(new Position(1,1));
+
+        controller.step(game, ACTION.UP, 1000L);
+
+        verify(floor, times(1)).movePlayer(new Position(1,1), eq(1000L));
+    }
+
+    @Test
+    void playerMovesDownCorrectly() {
+        when(player.getMovementSpeed()).thenReturn(3);
+        when(player.moveToward(LOOKING.DOWN)).thenReturn(new Position(1,1));
+
+        controller.step(game, ACTION.DOWN, 1000L);
+
+        verify(floor, times(1)).movePlayer(new Position(1,1), eq(1000L));
+    }
+
+    @Test
+    void playerMovesLeftCorrectly() {
+        when(player.getMovementSpeed()).thenReturn(3);
+        when(player.moveToward(LOOKING.LEFT)).thenReturn(new Position(1,1));
+
+        controller.step(game, ACTION.LEFT, 1000L);
+
+        verify(floor, times(1)).movePlayer(new Position(1,1), eq(1000L));
+    }
+
+    @Test
+    void playerMovesRightCorrectly() {
+        when(player.getMovementSpeed()).thenReturn(3);
+        when(player.moveToward(LOOKING.RIGHT)).thenReturn(new Position(1,1));
+
+        controller.step(game, ACTION.RIGHT, 1000L);
+
+        verify(floor, times(1)).movePlayer(new Position(1,1), eq(1000L));
+    }
+
+    @Test
+    void usingPotionBlocksMovement() {
+        when(player.moveToward(LOOKING.RIGHT)).thenReturn(new Position(1,1));
+
+        controller.step(game, ACTION.USE_POTION_0, 1000L);
+        controller.step(game, ACTION.RIGHT, 1600L);
+        verify(player, times(1)).consumeItem(0);
+        verify(floor, never()).movePlayer(any(Position.class), eq(1600L));
+    }
+
+    @Test
+    void unequippingWeaponBlocksMovement() {
+        when(player.moveToward(LOOKING.RIGHT)).thenReturn(new Position(1,1));
+
+        controller.step(game, ACTION.UNEQUIP_WEAPON, 1000L);
+        controller.step(game, ACTION.RIGHT, 1600L);
+        verify(player, times(1)).equipWeapon(null);
+        verify(floor, never()).movePlayer(any(Position.class), eq(1600L));
+    }
+
+    @Test
+    void unequippingHeadBlocksMovement() {
+        when(player.moveToward(LOOKING.RIGHT)).thenReturn(new Position(1,1));
+
+        controller.step(game, ACTION.UNEQUIP_HEAD, 1000L);
+        controller.step(game, ACTION.RIGHT, 1600L);
+        verify(player, times(1)).unequipArmour(ArmourSlot.HEAD);
+        verify(floor, never()).movePlayer(any(Position.class), eq(1600L));
+    }
+
+    @Test
+    void unequippingChestBlocksMovement() {
+        when(player.moveToward(LOOKING.RIGHT)).thenReturn(new Position(1,1));
+
+        controller.step(game, ACTION.UNEQUIP_CHEST, 1000L);
+        controller.step(game, ACTION.RIGHT, 1600L);
+        verify(player, times(1)).unequipArmour(ArmourSlot.CHEST);
+        verify(floor, never()).movePlayer(any(Position.class), eq(1600L));
+    }
+
+    @Test
+    void unequippingArmsBlocksMovement() {
+        when(player.moveToward(LOOKING.RIGHT)).thenReturn(new Position(1,1));
+
+        controller.step(game, ACTION.UNEQUIP_ARMS, 1000L);
+        controller.step(game, ACTION.RIGHT, 1600L);
+        verify(player, times(1)).unequipArmour(ArmourSlot.ARMS);
+        verify(floor, never()).movePlayer(any(Position.class), eq(1600L));
+    }
+
+    @Test
+    void unequippingLegsBlocksMovement() {
+        when(player.moveToward(LOOKING.RIGHT)).thenReturn(new Position(1,1));
+
+        controller.step(game, ACTION.UNEQUIP_LEGS, 1000L);
+        controller.step(game, ACTION.RIGHT, 1600L);
+        verify(player, times(1)).unequipArmour(ArmourSlot.LEGS);
+        verify(floor, never()).movePlayer(any(Position.class), eq(1600L));
+    }
+
+    @Test
+    void unequippingFeetBlocksMovement() {
+        when(player.moveToward(LOOKING.RIGHT)).thenReturn(new Position(1,1));
+
+        controller.step(game, ACTION.UNEQUIP_FEET, 1000L);
+        controller.step(game, ACTION.RIGHT, 1600L);
+        verify(player, times(1)).unequipArmour(ArmourSlot.FEET);
+        verify(floor, never()).movePlayer(any(Position.class), eq(1600L));
+    }
+    
 }
