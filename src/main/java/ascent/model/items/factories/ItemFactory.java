a1536e3e7c61971c56b81993be4a957705cbb553
dcl674 <up202306056@up.pt>
2025-12-28 16:26:19 +0000

    Corrected ItemFactory to kill mutants

--- src/main/java/ascent/model/items/factories/ItemFactory.java
+++ src/main/java/ascent/model/items/factories/ItemFactory.java
@@ -9,30 +9,31 @@ import ascent.model.items.armour.ArmourSlot;
 import java.util.Random;
 
 public class ItemFactory {
-    // We separated the ids between different types of items to make it easier to add new ones
+    // We separated the ids between different types of items to make it easier to
+    // add
+    // new ones
     // We created MIN e MAX to facilitate random number generation
-    // Potions between 1 and 10
+    // We added bounds to eliminate the need to use +1 when generating random
+    // numbers
     private static final int HEALTH_POTION_SMALL = 1;
     private static final int HEALTH_POTION_MEDIUM = 2;
     private static final int HEALTH_POTION_LARGE = 3;
     private static final int POTION_MIN = 1;
-    private static final int POTION_MAX = 3;
+    private static final int POTION_BOUND = 4;
     private static final int SWORD = 11;
 
-    // Weapons between 11 and 20
     private static final int AXE = 12;
     private static final int KNIFE = 13;
     private static final int WEAPON_MIN = 11;
-    private static final int WEAPON_MAX = 13;
+    private static final int WEAPON_BOUND = 14;
     private static final int HEAD_ARMOUR_BASIC = 21;
 
-    // armour between 21 and 30
     private static final int CHEST_ARMOUR_BASIC = 22;
     private static final int LEGS_ARMOUR_BASIC = 23;
     private static final int FEET_ARMOUR_BASIC = 24;
     private static final int ARMS_ARMOUR_BASIC = 25;
     private static final int ARMOUR_MIN = 21;
-    private static final int ARMOUR_MAX = 25;
+    private static final int ARMOUR_BOUND = 26;
     private static ItemFactory instance;
     private final Random random;
 
@@ -47,7 +48,7 @@ public class ItemFactory {
         return instance;
     }
 
-    public Item createItem(int id) throws InvalidItemIdException{
+    public Item createItem(int id) throws InvalidItemIdException {
         switch (id) {
             case HEALTH_POTION_SMALL:
                 return new HealthRestore(id, "Small Health Potion", 15);
@@ -64,7 +65,7 @@ public class ItemFactory {
                 return new Weapon(id, "Knife", 0, 5);
 
             case HEAD_ARMOUR_BASIC:
-                return new Armour(id, "Basic Helmet", 0,3, ArmourSlot.HEAD);
+                return new Armour(id, "Basic Helmet", 0, 3, ArmourSlot.HEAD);
             case CHEST_ARMOUR_BASIC:
                 return new Armour(id, "Basic Chestplate", -4, 8, ArmourSlot.CHEST);
             case LEGS_ARMOUR_BASIC:
@@ -79,25 +80,18 @@ public class ItemFactory {
         }
     }
 
-    //  Method to improve readability and simplicity of code
-    //  Makes it easier to generate random items
-
-    private int randomIdInRange(int min, int max) {
-        return min + random.nextInt(max - min + 1);
-    }
-
     public Weapon createRandomWeapon() {
-        int randomId = randomIdInRange(WEAPON_MIN, WEAPON_MAX);
+        int randomId = random.nextInt(WEAPON_MIN, WEAPON_BOUND);
         return (Weapon) createItem(randomId);
     }
 
     public Armour createRandomArmour() {
-        int randomId = randomIdInRange(ARMOUR_MIN, ARMOUR_MAX);
+        int randomId = random.nextInt(ARMOUR_MIN, ARMOUR_BOUND);
         return (Armour) createItem(randomId);
     }
 
     public HealthRestore createRandomPotion() {
-        int randomId = randomIdInRange(POTION_MIN, POTION_MAX);
+        int randomId = random.nextInt(POTION_MIN, POTION_BOUND);
         return (HealthRestore) createItem(randomId);
     }
 
