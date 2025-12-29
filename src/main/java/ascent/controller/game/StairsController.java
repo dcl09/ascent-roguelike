1ed27ee32f250615196d74ae44241ba0601f4109
dcl674 <up202306056@up.pt>
2025-12-29 18:27:16 +0000

    Added StairsControllerTest and killed all mutants in pitest

--- src/main/java/ascent/controller/game/StairsController.java
+++ src/main/java/ascent/controller/game/StairsController.java
@@ -4,17 +4,25 @@ import ascent.Game;
 import ascent.gui.ACTION;
 import ascent.model.entities.Player;
 import ascent.model.entities.Stairs;
-import ascent.model.game.floor.FileLevelBuilder;
+import ascent.model.game.floor.FileLevelFactory;
 import ascent.model.game.floor.Floor;
 import ascent.model.menu.Endscreen;
 import ascent.state.EndscreenState;
 import ascent.state.GameState;
 
-import java.io.File;
 import java.io.IOException;
 
 public class StairsController extends GameController {
-    public StairsController(Floor floor) {super (floor);}
+    private FileLevelFactory fileLevelFactory;
+
+    public StairsController(Floor floor) {
+        this(floor, new FileLevelFactory());
+    }
+
+    public StairsController(Floor floor, FileLevelFactory fileLevelFactory) {
+        super(floor);
+        this.fileLevelFactory = fileLevelFactory;
+    }
 
     @Override
     public void step(Game game, ACTION action, long time) throws IOException {
@@ -23,13 +31,12 @@ public class StairsController extends GameController {
         if (player.facing().equals(stairs.getPosition())) {
             game.popState();
             int newLevel = getModel().getCurrLevel() + 1;
-            String filepath = "levels/level" + newLevel + ".txt";
-            if (new File(filepath).exists()) {
-                FileLevelBuilder builder = new FileLevelBuilder(filepath);
-                player.setPosition(builder.findPlayerSpawn());
-                game.pushState(new GameState(builder.createFloor(player, newLevel)));
-            }else
+            try {
+                Floor newFloor = fileLevelFactory.createLevel(newLevel, player);
+                game.pushState(new GameState(newFloor));
+            } catch (IOException e) {
                 game.pushState(new EndscreenState(new Endscreen(true)));
+            }
         }
     }
 }
